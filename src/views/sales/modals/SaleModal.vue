81f7933a1242f62d66f4cb1ca9719eeb59e5dac4
Mush <mush.harutyunyan@hotmail.com>
2025-09-08 23:00:55 +0400

    finalize

--- src/views/sales/modals/SaleModal.vue
+++ src/views/sales/modals/SaleModal.vue
@@ -153,7 +153,7 @@
           <input v-model="form.repayment_months_quantity" type="number"
                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                  required/>
-          <p v-if="form.repayment_months_quantity && form.price">{{ $t('monthly_payment') }}: {{ price(Math.floor((form.price - (form.deposit ?? 0)) / form.repayment_months_quantity)) }}</p>
+          <p v-if="form.repayment_months_quantity && form.price">{{ $t('monthly_payment') }}: {{ price(Math.floor((form.price + (form.garage_price ?? 0) - (form.deposit ?? 0)) / form.repayment_months_quantity)) }}</p>
         </div>
         <div>
           <button v-if="!loading" type="button" @click="store"
