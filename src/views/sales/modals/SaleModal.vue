23ab8d54537b7ccf4f049bcdb1e01011a293432d
Mush <mush.harutyunyan@hotmail.com>
2025-09-08 22:54:36 +0400

    finalize

--- src/views/sales/modals/SaleModal.vue
+++ src/views/sales/modals/SaleModal.vue
@@ -86,7 +86,7 @@
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{
                 $t('garage_price')
               }} ({{ $t('total') }})</label>
-            <input v-model="form.garage_price" type="text"
+            <input v-model="form.garage_price" type="number"
                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                    required/>
           </div>
